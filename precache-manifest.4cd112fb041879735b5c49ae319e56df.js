50f6419d003c30606bbefc7492ef942386510420
alshymaai <git@github.com>
2022-05-06 20:22:02 +0200

    Updates

---
+++ precache-manifest.4cd112fb041879735b5c49ae319e56df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24b200863e47e09fe321",
+    "url": "/React_p/static/css/main.b1184d9c.chunk.css"
+  },
+  {
+    "revision": "24b200863e47e09fe321",
+    "url": "/React_p/static/js/main.24b20086.chunk.js"
+  },
+  {
+    "revision": "877ddeaf8032078f6663",
+    "url": "/React_p/static/js/1.877ddeaf.chunk.js"
+  },
+  {
+    "revision": "345d9c5e59bb27d71560",
+    "url": "/React_p/static/js/2.345d9c5e.chunk.js"
+  },
+  {
+    "revision": "a6b48813a26b2d28f7a1",
+    "url": "/React_p/static/js/runtime~main.a6b48813.js"
+  },
+  {
+    "revision": "1b91651e3f1d884a05e212538e4f8919",
+    "url": "/React_p/index.html"
+  }
+];
